199f025a60bfb39ae9554491ab12583a7934a93f
Saurav Prashar <61817653+Dynocoder@users.noreply.github.com>
2023-07-01 04:33:39 -0400

    Add files via upload

---
+++ Assignment_4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String suits[] = {"Club", "Spade", "Heart", "diamonds"};
+
+        System.out.println(suits[2]);
+    }
+}
